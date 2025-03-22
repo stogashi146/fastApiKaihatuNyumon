41df42830ca9a8eff830da281763e16783af0cfb
stogashi146 <stogashi146@gmail.com>
2025-03-22 00:38:19 +0000

    Chapter13 asyncioを使用して非同期化

--- api/cruds/done.py
+++ api/cruds/done.py
@@ -2,23 +2,24 @@ from sqlalchemy import Result, select
 from sqlalchemy.orm import Session
 
 import api.models.task as task_model
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
-def get_done(db: Session, task_id: int) -> task_model.Done | None:
-  result: Result = db.execute(
+async def get_done(db: AsyncSession, task_id: int) -> task_model.Done | None:
+  result: Result = await db.execute(
     select(task_model.Done).filter(task_model.Done.id == task_id)
   )
   
   return result.scalars().first()
 
-def create_done(db: Session, task_id: int) -> task_model.Done:
+async def create_done(db: AsyncSession, task_id: int) -> task_model.Done:
   done = task_model.Done(id = task_id)
   db.add(done)
-  db.commit()
-  db.refresh(done)
+  await db.commit()
+  await db.refresh(done)
   
   return done
 
-def delete_done(db: Session, original: task_model.Done) -> None:
-  db.delete(original)
-  db.commit()
+async def delete_done(db: AsyncSession, original: task_model.Done) -> None:
+  await db.delete(original)
+  await db.commit()
--- api/db.py
+++ api/db.py
@@ -1,15 +1,21 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 
 DB_URL = "mysql+pymysql://root@db:3306/demo?charset=utf8"
+ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/demo?charset=utf8"
 
-db_engine = create_engine(DB_URL, echo=True)
-db_session = sessionmaker(autocommit = False, autoflush=False, bind=db_engine)
+# db_engine = create_e1ngine(DB_URL, echo=True)
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+# db_session = sessionmaker(autocommit = False, autoflush=False, bind=db_engine)
+
+async_session = sessionmaker(bind = async_engine, autocommit=False, autoflush=False, class_=AsyncSession)
+
 
 Base = declarative_base()
 
-def get_db():
-  with db_session() as session:
+async def get_db():
+  async with async_session() as session:
       yield session
